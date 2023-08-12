f04dfe3756ed8f3aa232780e5a9b03d3f83e13f7
Oscar Fonseca <o.fonsecan@uniandes.edu.co>
2023-08-12 10:56:26 -0500

    Implementación de las asociaciones

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,5 +1,5 @@
 from flaskr import create_app
-from .modelos import db, Cancion
+from .modelos import db, Cancion, Usuario, Album, Medio
 
 app = create_app('default')
 app_context = app.app_context()
@@ -8,7 +8,24 @@ app_context.push()
 db.init_app(app)
 db.create_all()
 
-#PRUEBA
+with app.app_context():
+    u = Usuario(nombre='Juan', contrasena='12345')
+    a = Album(titulo='prueba', anio=1999, descripcion='texto', medio=Medio.CD)
+    c = Cancion(titulo='mi cancion', minutos=1, segundos=15, interprete='Juanito')
+    u.albumes.append(a)
+    a.canciones.append(c)
+    db.session.add(u)
+    db.session.add(c)
+    db.session.commit()
+    print(Album.query.all())
+    print(Album.query.all()[0].canciones)
+    print(Cancion.query.all())
+    db.session.delete(a)
+    print(Album.query.all())
+    print(Cancion.query.all())
+
+'''
+#PRUEBA Implementación de las clases
 with app.app_context():
     c = Cancion(titulo='Prueba', minutos=2, segundos=25, interprete='Juan Pablo')
     c2 = Cancion(titulo='Prueba2', minutos=2, segundos=25, interprete='Juan Pablo')
@@ -16,3 +33,4 @@ with app.app_context():
     db.session.add(c2)
     db.session.commit()
     print(Cancion.query.all())
+'''
--- flaskr/modelos/modelos.py
+++ flaskr/modelos/modelos.py
@@ -1,6 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
+import enum
 
 db = SQLAlchemy()
+
+albumes_canciones = db.Table('album_cancion',\
+    db.Column('album_id', db.Integer, db.ForeignKey('album.id'), primary_key=True),\
+    db.Column('cancion_id', db.Integer, db.ForeignKey('cancion.id'), primary_key=True))
+
 
 class Cancion(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,6 +14,31 @@ class Cancion(db.Model):
     minutos = db.Column(db.Integer)
     segundos = db.Column(db.Integer)
     interprete = db.Column(db.String(128))
+    albumes = db.relationship('Album', secondary='album_cancion', back_populates='canciones')
 
     def __repr__(self):
         return "{}-{}-{}-{}".format(self.titulo, self.minutos, self.segundos, self.interprete)
+    
+
+class Medio(enum.Enum):
+   DISCO = 1
+   CASETE = 2
+   CD = 3
+
+
+class Album(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(128))
+    anio = db.Column(db.Integer)
+    descripcion = db.Column(db.String(256))
+    medio = db.Column(db.Enum(Medio))
+    usuario = db.Column(db.Integer, db.ForeignKey('usuario.id'))
+    canciones = db.relationship('Cancion', secondary='album_cancion', back_populates='albumes')
+    __table_args__ = (db.UniqueConstraint('usuario', 'titulo', name='titulo_unico_album'),)
+
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(64))
+    contrasena = db.Column(db.String(32))
+    albumes = db.relationship('Album', cascade='all, delete, delete-orphan')
