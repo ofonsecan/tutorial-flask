89e76236dfb1971f401607fe94658dc3db630aac
Oscar Fonseca <o.fonsecan@uniandes.edu.co>
2023-08-12 12:54:17 -0500

    Pruebas a la aplicación

--- flaskr/app.py
+++ flaskr/app.py
@@ -2,7 +2,7 @@ from flaskr import create_app
 from .modelos import db, Cancion, Album, Usuario, Medio
 from .modelos import AlbumSchema
 from flask_restful import Api
-from .vistas import VistaCanciones, VistaCancion
+from .vistas import VistaCanciones, VistaCancion, VistaSignIn, VistaLogIn, VistaAlbum, VistaAlbumsUsuario, VistaCancionesAlbum
 
 app = create_app('default')
 app_context = app.app_context()
@@ -14,6 +14,11 @@ db.create_all()
 api = Api(app)
 api.add_resource(VistaCanciones, '/canciones')
 api.add_resource(VistaCancion, '/cancion/<int:id_cancion>')
+api.add_resource(VistaSignIn, '/signin')
+api.add_resource(VistaLogIn, '/login')
+api.add_resource(VistaAlbumsUsuario, '/usuario/<int:id_usuario>/albumes')
+api.add_resource(VistaAlbum, '/album/<int:id_album>')
+api.add_resource(VistaCancionesAlbum, '/album/<int:id_album>/canciones')
 
 '''
 #PRUEBA Serialización de los objetos
--- flaskr/vistas/vistas.py
+++ flaskr/vistas/vistas.py
@@ -1,8 +1,11 @@
 from flask_restful import Resource
-from ..modelos import db, Cancion, CancionSchema
+from ..modelos import db, Cancion, CancionSchema, Usuario, UsuarioSchema, Album, AlbumSchema
 from flask import request
+from sqlalchemy.exc import IntegrityError
 
 cancion_schema = CancionSchema()
+usuario_schema = UsuarioSchema()
+album_schema = AlbumSchema()
 
 class VistaCanciones(Resource):
 
@@ -38,3 +41,100 @@ class VistaCancion(Resource):
         db.session.delete(cancion)
         db.session.commit()
         return 'Operacion exitosa', 204
+    
+
+class VistaLogIn(Resource):
+    def post(self):
+            u_nombre = request.json["nombre"]
+            u_contrasena = request.json["contrasena"]
+            usuario = Usuario.query.filter_by(nombre=u_nombre, contrasena = u_contrasena).all()
+            if usuario:
+                return {'mensaje':'Inicio de sesión exitoso'}, 200
+            else:
+                return {'mensaje':'Nombre de usuario o contraseña incorrectos'}, 401
+
+
+class VistaSignIn(Resource):
+    
+    def post(self):
+        nuevo_usuario = Usuario(nombre=request.json["nombre"], contrasena=request.json["contrasena"])
+        db.session.add(nuevo_usuario)
+        db.session.commit()
+        return 'Usuario creado exitosamente', 201
+
+    def put(self, id_usuario):
+        usuario = Usuario.query.get_or_404(id_usuario)
+        usuario.contrasena = request.json.get("contrasena",usuario.contrasena)
+        db.session.commit()
+        return usuario_schema.dump(usuario)
+
+    def delete(self, id_usuario):
+        usuario = Usuario.query.get_or_404(id_usuario)
+        db.session.delete(usuario)
+        db.session.commit()
+        return '',204
+
+
+class VistaAlbumsUsuario(Resource):
+
+    def post(self, id_usuario):
+        nuevo_album = Album(titulo=request.json["titulo"], anio=request.json["anio"], descripcion=request.json["descripcion"], medio=request.json["medio"])
+        usuario = Usuario.query.get_or_404(id_usuario)
+        usuario.albumes.append(nuevo_album)
+
+        try:
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+            return 'El usuario ya tiene un album con dicho nombre',409
+
+        return album_schema.dump(nuevo_album)
+
+    def get(self, id_usuario):
+        usuario = Usuario.query.get_or_404(id_usuario)
+        return [album_schema.dump(al) for al in usuario.albumes]
+
+
+class VistaCancionesAlbum(Resource):
+
+    def post(self, id_album):
+        album = Album.query.get_or_404(id_album)
+        
+        if "id_cancion" in request.json.keys():
+            
+            nueva_cancion = Cancion.query.get(request.json["id_cancion"])
+            if nueva_cancion is not None:
+                album.canciones.append(nueva_cancion)
+                db.session.commit()
+            else:
+                return 'Canción errónea',404
+        else: 
+            nueva_cancion = Cancion(titulo=request.json["titulo"], minutos=request.json["minutos"], segundos=request.json["segundos"], interprete=request.json["interprete"])
+            album.canciones.append(nueva_cancion)
+        db.session.commit()
+        return cancion_schema.dump(nueva_cancion)
+       
+    def get(self, id_album):
+        album = Album.query.get_or_404(id_album)
+        return [cancion_schema.dump(ca) for ca in album.canciones]
+
+
+class VistaAlbum(Resource):
+
+    def get(self, id_album):
+        return album_schema.dump(Album.query.get_or_404(id_album))
+
+    def put(self, id_album):
+        album = Album.query.get_or_404(id_album)
+        album.titulo = request.json.get("titulo",album.titulo)
+        album.anio = request.json.get("anio", album.anio)
+        album.descripcion = request.json.get("descripcion", album.descripcion)
+        album.medio = request.json.get("medio", album.medio)
+        db.session.commit()
+        return album_schema.dump(album)
+
+    def delete(self, id_album):
+        album = Album.query.get_or_404(id_album)
+        db.session.delete(album)
+        db.session.commit()
+        return '',204
